c45ea36e8ce6a665061464826d3c7683b5489b12
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-06 14:56:02 +0000

    Sep 6, 2026, 4:56 PM

---
+++ SD_Model(no pump).m
@@ -0,0 +1,13 @@
+%This section only contains SD Model with no pump
+
+i = 2;                          % NaCl fracction
+R = 8.314;                      % Gas constant [J/mol.K]
+T_c = 25;                       % Temp [C]
+T = T_c + 273.15;               % Temp [K]
+C_f_gL = 35;                    % Feed Salinity [g/L]
+Mw = 58.44;                     %
+C_f = 
+
+
+
+delta_pi = i*R*T*C_f            % Osmotic Pressure
