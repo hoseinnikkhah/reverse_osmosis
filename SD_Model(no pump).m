876b9e0a3e31e3c8b7774b8e353a8782a35e748f
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-06 14:58:54 +0000

    Sep 6, 2026, 4:58 PM

--- SD_Model(no pump).m
+++ SD_Model(no pump).m
@@ -5,8 +5,8 @@ R = 8.314;                      % Gas constant [J/mol.K]
 T_c = 25;                       % Temp [C]
 T = T_c + 273.15;               % Temp [K]
 C_f_gL = 35;                    % Feed Salinity [g/L]
-Mw = 58.44;                     %
-C_f = 
+Mw = 58.44;                     % Molecular weight
+C_f = C_f_gL/Mw;                % Feed Salinity [mol/L]
 
 
 
