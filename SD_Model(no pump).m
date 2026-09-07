5c48df5c65c04a72ce3d274cf2fa67e5d6f1be8c
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-07 14:11:32 +0200

    svae

--- SD_Model(no pump).m
+++ SD_Model(no pump).m
@@ -12,3 +12,12 @@ C_f = C_f_gL/Mw;                % Feed Salinity [mol/L]
 
 delta_pi = i*R*T*C_f            % Osmotic Pressure [J/L] or [kPa]
 delta_pi_bar = delta_pi/100;    % Osmotic Pressure [bar]
+
+% A = (D_w*K_w*V_w*c_w)/(l*R*T)
+% In above once at x=0 and once x=l calculation are carried
+
+V_w = 18;                       % Partial molar volume of water [cm3/mol]
+c_w = [0,0];                    % Water concentration in the membrane (at feed, after membrane)
+l = 100;                        % Membrane thickness [nm]
+
+% Data sheet data (DuPont FilmTec SW30HRLE-440)
