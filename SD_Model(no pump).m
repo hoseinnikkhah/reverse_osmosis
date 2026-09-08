a62a425b1afcec0325108452633441eea4e8aa1d
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-08 18:55:27 +0000

    Sep 8, 2026, 8:55 PM

--- SD_Model(no pump).m
+++ SD_Model(no pump).m
@@ -1,13 +1,19 @@
-%This section only contains SD Model with no pump
+%% This code is essential part of the SD model. It calculates the osmotic pressure
+and water permeability of the membrane. This is based on commercial data sheet of
+DuPont FilmTec SW30HRLE-440. The code is used in the SD model to calculate the 
+water flux through the membrane. Based on the water flux, A and B coeffecients are calculated.
 
+
+% Data driven from the data sheet of DuPont FilmTec SW30HRLE-440
+% Note NaCl fraction is 2 for NaCl, 3 for CaCl2 and 4 for MgCl2
 i = 2;                          % NaCl fracction
-R = 8.314;                      % Gas constant [J/mol.K]
 T_c = 25;                       % Temp [C]
 T = T_c + 273.15;               % Temp [K]
-C_f_gL = 35;                    % Feed Salinity [g/L]
+ppm = 32000;                    % Feed Salinity [ppm]
+C_f_gL = ppm/1000;              % Feed Salinity [g/L]
 Mw = 58.44;                     % Molecular weight
 C_f = C_f_gL/Mw;                % Feed Salinity [mol/L]
-
+R = 8.314;                      % Gas constant [J/mol.K]
 
 
 delta_pi = i*R*T*C_f            % Osmotic Pressure [J/L] or [kPa]
