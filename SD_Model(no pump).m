7ed82e3f55d1fd0b9e2c2a0b2ec7b32af40c0bab
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-08 19:17:33 +0000

    Sep 8, 2026, 9:17 PM

--- SD_Model(no pump).m
+++ SD_Model(no pump).m
@@ -6,23 +6,29 @@ water flux through the membrane. Based on the water flux, A and B coeffecients
 are calculated.
 %}
 
+% Note NaCl fraction is 2 for NaCl, 3 for CaCl2 and 4 for MgCl2
 
 % Data driven from the data sheet of DuPont FilmTec SW30HRLE-440
-% Note NaCl fraction is 2 for NaCl, 3 for CaCl2 and 4 for MgCl2
-i = 2;                          % NaCl fracction
+i = 2;                          % NaCl fracction 
 T_c = 25;                       % Temp [C]
-T = T_c + 273.15;               % Temp [K]
 ppm = 32000;                    % Feed Salinity [ppm]
-C_f_gL = ppm/1000;              % Feed Salinity [g/L]
+
+% User input data
 Mw = 58.44;                     % Molecular weight
+
+% Automated conversions
+T = T_c + 273.15;               % Temp [K]
+C_f_gL = ppm/1000;              % Feed Salinity [g/L]
 C_f = C_f_gL/Mw;                % Feed Salinity [mol/L]
+
+% Constant values
 R = 8.314;                      % Gas constant [J/mol.K]
 
-
+% Formula for osmotic pressure calculation
 delta_pi = i*R*T*C_f            % Osmotic Pressure [J/L] or [kPa]
 delta_pi_bar = delta_pi/100;    % Osmotic Pressure [bar]
 
-% A = (D_w*K_w*V_w*c_w)/(l*R*T)
+
 % In above once at x=0 and once x=l calculation are carried
 
 V_w = 18;                       % Partial molar volume of water [cm3/mol]
