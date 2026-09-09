39b3c690ce2040f6a946cb3d4701f57a9e827dc1
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-09 19:10:10 +0000

    Sep 9, 2026, 9:10 PM

--- A_tempature_model.m
+++ A_tempature_model.m
@@ -1,5 +1,5 @@
 load('membrane_params.mat', 'A', 'B', 'A_LMH','B_LMH');
-T_list = 20:1:35;               % Temperature range [C]
+T_list = 14:0.1:35;               % Temperature range [C]
 A_list = zeros(1,length(T_list));
 A_list_LMH = zeros(1,length(T_list));
 
