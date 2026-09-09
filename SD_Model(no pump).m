2f9284d63ded01ccfaabb178cd0e88908f247320
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-09 10:53:50 +0000

    Sep 9, 2026, 12:53 PM

--- SD_Model(no pump).m
+++ SD_Model(no pump).m
@@ -15,12 +15,12 @@ ppm = 32000;                    % Feed Salinity [ppm]
 
 A_mem = 41;                     % Active area [m2] A_mem
 Q_p = 30.2;                     % Permeate flow rate [m3/d]
-Rejection = 99.8;               % Salt rejection [%] R_salt = 1 - (C_p/C_f)*100 
-delta_P = 55;                   % Pressure drop across the membrane [bar]
-Recovery = 8;                   % Recovery [%]
+Rejection = 99.8;               % Salt rejection [%] R_salt = (1 - C_p/C_f)*100 
+delta_P = 55;                   % Applied feed pressure [bar]
+Recovery = 8;                   % Recovery [%] (test condition, not used below)
 
 % User input data
-Mw = 58.44;                     % Molecular weight
+Mw = 58.44;                     % Molecular weight [g/mol]
 
 % Automated conversions
 T = T_c + 273.15;               % Temp [K]
@@ -33,15 +33,20 @@ R = 8.314;                      % Gas constant [J/mol.K]
 % Salt rejection calculation
 C_p_gL = C_f_gL*(1 - Rejection/100);  % Permeate concentration [g/L]
 
-deltac_c = C_f_gL - C_p_gL;           % Concentration difference [g/L]
+deltac_c = C_f_gL - C_p_gL;           % Concentration difference [g/L] = [kg/m3]
 
 % Formula for osmotic pressure calculation
 delta_pi = i*R*T*C_f;                 % Osmotic pressure [Pa]
+delta_pi_bar = delta_pi/1e5;          % Osmotic pressure [bar]
 
 % Formula for flux calculation
-J_w = (Q_p/A_mem);                    % Water flux [m3/m2.d] or [LMH]
-J_s = J_w*C_p_gL;                     % Salt flux [g/m2.d]
+J_w = (Q_p/A_mem);                    % Water flux [m/d]
+J_s = J_w*C_p_gL;                     % Salt flux [kg/m2.d]
 
 % Formula for water permeability calculation
-A = J_w/(delta_P - delta_pi);         % Water permeability [m3/m2.d.bar] or [LMH/bar]
-B = J_s/deltac_c;                     % Salt permeability [m/d] or [LMH]
+A = J_w/(delta_P - delta_pi_bar);     % Water permeability [m/d.bar]
+B = J_s/deltac_c;                     % Salt permeability [m/d]
+
+% Sanity check against literature values (typical SWRO: A ~ 1.1, B ~ 0.06)
+A_LMH = A*1000/24;                    % Water permeability [LMH/bar]
+B_LMH = B*1000/24;                    % Salt permeability [LMH]
