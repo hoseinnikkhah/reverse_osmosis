8dc5525dd4a6966379b16746a33ab92ccaec1ba4
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-09 13:37:54 +0000

    Sep 9, 2026, 3:37 PM

--- A_B_tempature.m
+++ A_B_tempature.m
@@ -1,7 +1,12 @@
 load('membrane_params.mat', 'A', 'B', 'A_LMH','B_LMH');
 T_list = 20:1:35;               % Temperature range [C]
 A_list = zeros(1,length(T_list));
-for i=1:length(T_list)
-    i
+A_list_LMH = zeros(1,length(T_list));
 
+for i=1:length(T_list)
+    T_c = T_list(i);
+    A_temp_LMH = A_LMH*1.03^(T_c - 25);  % Temperature correction for A [LMH/bar]
+    A_list_LMH(i) = A_temp_LMH;
+    A_temp = A*1.03^(T_c - 25);  % Temperature correction for A [LMH/bar]
+    A_list(i) = A_temp_LMH;
 end
