98ffd13b33d74a41d8d50b0bc91daf4ee6b8eca9
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-10 14:29:16 +0000

    Sep 10, 2026, 4:29 PM

--- delta_P_finder.m
+++ delta_P_finder.m
@@ -1,3 +1,3 @@
 load('osmotic_pressure.mat', 'delta_pi_bar');
-save('salinity_and_temp_mean.mat', 'T_daily_mean', 'S_daily_mean', 'dates');
+load('salinity_and_temp_mean.mat', 'T_daily_mean', 'S_daily_mean', 'dates');
 load('A_temp_data.mat', 'A_list_LMH');
