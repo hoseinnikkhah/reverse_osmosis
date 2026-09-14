de2177b3e8a8cbe70d6b8df6fedefe6de678e7cd
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-14 10:14:00 +0000

    Sep 14, 2026, 12:14 PM

--- J_s_finder.m
+++ J_s_finder.m
@@ -7,3 +7,5 @@ dates = datetime(2025, 7, 31) : datetime(2026, 7, 31);
 
 S_daily_mean_mgL = S_daily_mean*1027;       % Salinity [mg/L]
 J_s = B_LMH*S_daily_mean_mgL;               % Salt flux [mg/m2.h]
+
+save('J_s_finder.mat', 'J_s', 'dates');
