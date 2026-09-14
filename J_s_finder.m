d0394772f40089422451283bcbf9fd03a8e10a51
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-14 09:59:41 +0000

    Sep 14, 2026, 11:59 AM

--- J_s_finder.m
+++ J_s_finder.m
@@ -1,5 +1,6 @@
 %% This code is used to find ouut J_s based on 366 values of Salinity
 %% This is indepdent of water temperature and water flux.
 
-load('salinity_and_temp_mean.mat', 'S_daily_mean', 'dates');
+load('salinity_and_temp_mean.mat', 'S_daily_mean');
 load('membrane_params.mat', 'B', 'B_LMH');
+dates = datetime(2025, 7, 31) : datetime(2026, 7, 31);
