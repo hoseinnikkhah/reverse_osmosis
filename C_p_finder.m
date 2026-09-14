431a8f425b53c40b424c3d9db7874fc1707a4098
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-14 10:33:23 +0000

    Sep 14, 2026, 12:33 PM

--- C_p_finder.m
+++ C_p_finder.m
@@ -3,3 +3,18 @@
 load('J_s_finder.mat', 'J_s', 'dates');
 J_w = 0:0.1:30;  % Water flux range [LMH]
 
+C_p_block = zeros(length(J_w), 366);
+
+for i = 1:length(J_w)
+    J_w_i = J_w(i);
+    for j = 1:366
+        J_s_j = J_s(j);
+        if J_w_i > 0
+            C_p_block(i, j) = J_s_j / J_w_i;  % Calculate C_p for each J_w and J_s
+        else
+            C_p_block(i, j) = NaN;  % Avoid division by zero
+        end
+    end
+end
+
+save('C_p_finder.mat', 'C_p_block');
