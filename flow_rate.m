b3a6342aca9e11e2432884a3666aa2727e5bcb48
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-21 13:33:19 +0000

    Sep 21, 2026, 3:33 PM

--- flow_rate.m
+++ flow_rate.m
@@ -4,5 +4,7 @@ N_elements = [6,7,8]; % Number of membranes in the vessel which is limited to 6-
 N_vessels = 1:10; % Number of vessels in the system which is from 1 to 10 vessels
 A_m = 41; % Active area of the membrane [m2] A_mem
 
+
 A_membrane = A_m .* N_elements; % Total active area of the membrane in the vessel [m2]
-total_A = A_membrane .* N_vessels; % Total active area of the membrane in the system [m2]
+
+%total_A = A_membrane .* N_vessels; % Total active area of the membrane in the system [m2]
