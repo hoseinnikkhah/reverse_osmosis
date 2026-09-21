1d29cbae2b2b87f719f876fa1ec70a41f6607878
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-21 14:17:08 +0000

    Sep 21, 2026, 4:17 PM

--- flow_rate.m
+++ flow_rate.m
@@ -1,29 +1,33 @@
-%% vessel design contains the design of the vessel and the number of membranes
+%% Vessel design
+A_m        = 41;                    % Active area per element [m2] (SW30HRLE-440)
+N_elements = [5, 6, 7];             % Elements per vessel (Codeline 80E max = 7)
+N_vessels  = 1:10;                  % Number of vessels in parallel
 
-N_elements = [6,7,8]; % Number of membranes in the vessel which is limited to 6-8 elements
-N_vessels = 1:10; % Number of vessels in the system which is from 1 to 10 vessels
-A_m = 41; % Active area of the membrane [m2] A_mem
+% Total active area: rows = elements per vessel, columns = number of vessels
+total_A = A_m * N_elements(:) * N_vessels;          % [m2], size 3 x 10
 
-
-A_membrane = A_m .* N_elements; % Total active area of the membrane in the vessel [m2]
-
-total_A = zeros(length(N_elements), length(N_vessels)); % Preallocate total_A array
-for i = 1:length(N_elements)
-    for j = 1:length(N_vessels)
-        total_A(i, j) = A_membrane(i) * N_vessels(j); % Total active area of the system [m2]
-    end
-end
-
-%% Flux ranges for different pretreatment and membrane configurations
-%flux_range_UF_plus_B_free = 17:0.5:21; % Range of flux values for UF + B-free
-%flux_range_Well_open_intake_plus_UF = 15:0.5:19; % Range of flux values for Well/open intake + UF
-%flux_range_Generic_membrane_filtration = 14:0.5:17; % Range of flux values for Generic membrane filtration
-%flux_range_Conventional_pretreatment = 12:0.5:17; % Range of flux values for Conventional pretreatment
-
-Name    = ["UF + B-free"; "Well/open intake + UF"; ...
-           "Generic membrane filtration"; "Conventional pretreatment"];
-J_design = {17:0.5:21; 15:0.5:19; 14:0.5:17; 12:0.5:17};
-J_max    = [38; 36; 34; 32];
-rec_max  = [16; 15; 14; 13];
+%% Pretreatment configurations (DuPont FilmTec Manual, Table 22, seawater)
+Name     = ["UF + B-free"; "Well/open intake + UF"; ...
+            "Generic membrane filtration"; "Conventional pretreatment"];
+J_design = {17:0.5:21; 15:0.5:19; 14:0.5:17; 12:0.5:17};   % Design flux [LMH]
+J_max    = [38; 36; 34; 32];                                % Max element flux [LMH]
+rec_max  = [16; 15; 14; 13];                                % Max element recovery [%]
 
 config = table(Name, J_design, J_max, rec_max);
+
+%% Permeate production for every design and every design flux
+% Q_p{k} has size [elements x vessels x flux] for configuration k
+LMH_to_m3d = 24/1000;               % L/(m2.h) x m2 -> m3/d
+
+Q_p = cell(height(config), 1);
+for k = 1:height(config)
+    J3     = reshape(config.J_design{k}, 1, 1, []);   % flux along 3rd dimension
+    Q_p{k} = total_A .* J3 * LMH_to_m3d;               % [m3/d]
+end
+
+%% Sanity check: conventional pretreatment, 7 elements, 5 vessels, 14 LMH
+k  = find(config.Name == "Conventional pretreatment");
+iE = find(N_elements == 7);
+iV = find(N_vessels == 5);
+iJ = find(config.J_design{k} == 14);
+fprintf('Q_p = %.1f m3/d (expected ~482)\n', Q_p{k}(iE, iV, iJ));
