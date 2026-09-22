b726dc28f12fe0d861a58f218ddbda0a7f1d22f4
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-22 15:26:24 +0000

    Sep 22, 2026, 5:26 PM

--- recovery_flow_rate.m
+++ recovery_flow_rate.m
@@ -1,11 +1,12 @@
 data = load('design_data.mat');
 
-ceil_rec = data.ceil_recovery;      % 3×4 matrix
-recovery_rate = 30:1:60;            % Recovery rate range [%]
+ceil_rec = data.ceil_recovery;        % 3×4 matrix
+recovery_rate_1 = 30:1:60;            % Recovery rate range [%]
+recovery_rate_2 = 30:1:50;            % Recovery rate range [%]
 
 for i = 1:3
     for j = 1:4
-        if ceil_rec(i,j) < recovery_rate(end)/100
-            fprintf('Configuration %d, Elements per vessel %d: Max recovery %.2f%% < %.2f%%\n')
-                    recovery_rate_temp = 30:1:50;  % Recovery rate range [%]
+        if ceil_rec(i,j) < recovery_rate_1(end)/100
+          break
         end
+
