642a7bd871a1eb886028f09db1c254fe9c8fe841
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-22 17:00:03 +0000

    Sep 22, 2026, 7:00 PM

--- recovery_flow_rate.m
+++ recovery_flow_rate.m
@@ -12,9 +12,22 @@ Q_Well_open_intake_UF = data.Q_p{2, 1};
 Q_Generic_membrane_filtration = data.Q_p{3, 1};
 Q_Conventional_pretreatment = data.Q_p{4, 1};
 
-%for i = 1:3
-%    for j = 1:4
-%        if ceil_rec(i,j) < recovery_rate_1(end)/100
-%          break
-%        end
-
+for i = 1:3
+    for j = 1:10
+        for k = 1:9
+        if ceil_rec(i,1) > recovery_rate_1(end)
+            Q_UF_B_free(i, j, k) ./ recovery_rate_1;
+        elseif ceil_rec(i,1) < recovery_rate_1(end)
+            Q_UF_B_free(i, j, k) ./ recovery_rate_2;
+        if ceil_rec(i,2) > recovery_rate_1(end)
+            Q_Well_open_intake_UF(i, j, k) ./ recovery_rate_1;
+        elseif ceil_rec(i,2) < recovery_rate_1(end)
+            Q_Well_open_intake_UF(i, j, k) ./ recovery_rate_2;
+        if ceil_rec(i,3) > recovery_rate_1(end)
+            Q_Generic_membrane_filtration(i, j, k) ./ recovery_rate_1;
+        elseif ceil_rec(i,3) < recovery_rate_1(end)
+            Q_Generic_membrane_filtration(i, j, k) ./ recovery_rate_2;
+        if ceil_rec(i,4) > recovery_rate_1(end)
+            Q_Conventional_pretreatment(i, j, k) ./ recovery_rate_1;
+        elseif ceil_rec(i,4) < recovery_rate_1(end)
+            Q_Conventional_pretreatment(i, j, k) ./ recovery_rate_2;
