a7bb2203bd0bdbd881a197e65ecc28b42ee4b873
Hosein Nikkhah <hossein.nikkhah90@gmail.com>
2026-09-23 17:13:57 +0000

    Sep 23, 2026, 7:13 PM

---
+++ brine_conc.m
@@ -0,0 +1,9 @@
+load('salinity_and_temp_mean.mat')
+recovery = 30:1:50;                                             % Recovery range [%]
+C_brine = zeros(length(recovery), length(S_daily_mean));        % Preallocate brine concentration array
+
+for i = 1:length(recovery)
+    for j = 1:length(S_daily_mean)
+        C_brine(i, j) = S_daily_mean(j) / (1 - recovery(i)/100);  % Calculate brine concentration
+    end
+end
